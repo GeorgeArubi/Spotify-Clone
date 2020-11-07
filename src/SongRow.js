9089a74c5b5b0f1cc9be0dc8c7637d43e8233b08
DOGHOUSETV <arubigeorge0@gmail.com>
2020-11-07 01:41:38 -0500

    commit

--- src/SongRow.js
+++ src/SongRow.js
@@ -3,14 +3,27 @@ import './SongRow.css'
 
 function SongRow({ track }) {
     return (
-        <div className="songRow">
-            <img className="songRow__album" src={track.album.images[0].url} alt=""/>
-            <div className="songRow__info">
-                <h1>{track.name}</h1>
-                <p>
-                    {track.artists.map((artist) => artist.name).join(", ")} -{" "}
-                    {track.album.name}
-                </p>
+        <div className="songColumn">
+            <div className="column">
+                <div className="songTitle">
+                    <img className="songTitle__album" src={track.album.images[0].url} alt=""/>
+                    <div className="songTitle__info">
+                        <h1>{track.name}</h1>
+                        <p>
+                            {track.artists.map((artist) => artist.name)}
+                        </p>
+                    </div>
+                </div>
+             </div>
+            <div className="column">
+                <div className="trackAlbum">
+                <p>{track.album.name}</p>
+                </div>
+            </div>
+            <div className="column">
+                <div className="trackDuration">
+                    <p>{track.duration_ms}</p>
+                </div>
             </div>
         </div>
     )
